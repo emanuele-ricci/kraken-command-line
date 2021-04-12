bb6ccb9f0b2a75d2b2ec4b56b90afca2512151d7
emanuele-ricci <82324327+emanuele-ricci@users.noreply.github.com>
2021-04-12 02:22:58 +0200

    Add files via upload

---
+++ kraken.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+arguments=();
+
+args=("$@");
+n_args=${#args[@]};
+
+for (( i=0;i<$n_args;i++)); do
+    arguments+=(${args[${i}]})
+done
+ 
+
+info(){
+    echo "Options available:"
+    echo "[option] [sell/buy action] [pair from asset.json]"
+    echo ""
+	echo "-m [buy] [XBT/EUR] [volume] [leverage] [priceClose]                              set order sell or buy with open market price and limit closing price"
+    echo "-l [sell] [XBT/EUR] [volume] [leverage] [priceOpen] [priceClose]                 set order sell or buy with limit open price and closing price"
+    echo "-p [XBT/EUR]                                                                     current price and last min,max 24h prices"
+    echo "-b                                                                               balance available"
+    echo "-o                                                                               list all txid open orders"
+    echo "-i [txid]                                                                        get order info from txid"
+    echo "-d [txid]                                                                        delete an order from txid"
+}
+
+
+if [[ -z $1 ]] || [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
+    info
+else
+    node "./query_modules/kraken-command.js" ${arguments[@]}
+fi
